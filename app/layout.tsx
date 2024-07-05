2e65989e9b08e5bd3497598522fddd45c4254ff0
nafisholeh <rachmadnafisholeh@gmail.com>
2024-07-05 13:53:04 +0700

    fix: update web title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Event Genius",
+  description: "The ultimate AI-powered assistant designed to make event planning a breeze!",
 };
 
 export default function RootLayout({
