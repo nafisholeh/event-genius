3bb483ea54df44ab34f108f8e5a60ea745534383
nafisholeh <rachmadnafisholeh@gmail.com>
2024-07-09 19:14:23 +0700

    fix: add highlight to active session

--- app/protected/components/ChatSessions.tsx
+++ app/protected/components/ChatSessions.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { SessionContext } from "../contexts/SessionContext";
 
 export default function ChatSessions() {
-  const { sessions, setSessionId, addSession } = useContext(SessionContext);
+  const { sessions, sessionId, setSessionId, addSession } = useContext(SessionContext);
 
   return (
     <div>
@@ -21,7 +21,7 @@ export default function ChatSessions() {
           ? sessions.map((session) => (
               <button
                 key={session}
-                className="w-full p-2 text-left bg-gray-100 rounded-lg"
+                className={`${sessionId === session ? "bg-black text-white" : "bg-gray-100"} w-full p-2 text-left rounded-lg`}
                 onClick={() => setSessionId(session)}>
                 Chat with Session {session}
               </button>
